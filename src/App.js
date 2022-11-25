c3a5a483c9445e2e34cedd654f23fc6e4bac314e
Hernan Valencia <frjr17@criptext.com>
2022-11-25 00:36:08 -0500

    Shallow merges

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     super();
 
     this.state = {
-      name:"Adrien"
+      name: {firstName:"Adrien",lastName:'Valencia'},
+      company:'ZTM'
     }
   }
 
@@ -17,8 +18,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name}</p>
-          <button>Change name</button>
+          <p>Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}</p>
+          <button onClick={()=>{this.setState({name:{firstName:'Hernan', lastName:"Dominguez"}})}}>Change name</button>
         </header>
       </div>
     )
