bda3fb304c450e40270fd7d7680fa5351a8abebe
Hernan Valencia <frjr17@criptext.com>
2022-11-26 18:34:38 -0500

    Storing original data

--- src/App.js
+++ src/App.js
@@ -1,49 +1,49 @@
-import { Component } from 'react';
-
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: [
+      monsters: [],
+      search: ''
+    };
+  }
 
-        {
-          name: "Linda",
-          id:123
-        },
-        {
-          name: "Frank",
-          id:234
-        },
-        {
-          name: "Jacky",
-          id:345
-        },
-        {
-          name: "Adrien",
-          id:456
-        },
-      ]
-    }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(r => r.json())
+      .then(users =>
+        this.setState(() => { return { monsters: users, filteredMonsters: users } }))
+      .catch(console.error)
   }
 
   render() {
+    const filteredMonsters = this.state.monsters.filter(m =>
+      m.name.toLowerCase().includes(this.state.search.toLowerCase()))
+
     return (
       <div className="App">
-        {
-          this.state.monsters.map((monster) => {
-            return <div key={monster.id}>
+        <input
+          className='search-box'
+          type='search'
+          placeholder="Search a monster here"
+          value={this.state.search}
+          onChange={(event) => {
+            const { value } = event.target;
+            this.setState({ search: value })
+          }} />
+        {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
-          })
-        }
+          );
+        })}
       </div>
-    )
+    );
   }
 }
-
 
 export default App;
