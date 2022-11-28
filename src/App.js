9ce781db207e734bfb71611c141309647d725384
Hernan Valencia <frjr17@criptext.com>
2022-11-28 14:00:36 -0500

    Card List

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import "./App.css";
+import CardList from "./components/CardList/CardList";
 
 class App extends Component {
   constructor() {
@@ -7,40 +8,51 @@ class App extends Component {
 
     this.state = {
       monsters: [],
-      search: ''
+      search: "",
     };
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(r => r.json())
-      .then(users =>
-        this.setState(() => { return { monsters: users, filteredMonsters: users } }))
-      .catch(console.error)
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((r) => r.json())
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users, filteredMonsters: users };
+        })
+      )
+      .catch(console.error);
   }
 
+  onSearchChange = (event) => {
+    const { value } = event.target;
+    this.setState({ search: value });
+  };
+
   render() {
-    const filteredMonsters = this.state.monsters.filter(m =>
-      m.name.toLowerCase().includes(this.state.search.toLowerCase()))
+    const { monsters, search } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((m) =>
+      m.name.toLowerCase().includes(search.toLowerCase())
+    );
 
     return (
       <div className="App">
         <input
-          className='search-box'
-          type='search'
+          className="search-box"
+          type="search"
           placeholder="Search a monster here"
-          value={this.state.search}
-          onChange={(event) => {
-            const { value } = event.target;
-            this.setState({ search: value })
-          }} />
-        {filteredMonsters.map((monster) => {
+          value={search}
+          onChange={onSearchChange}
+        />
+        {/* {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
           );
-        })}
+        })} */}
+        <CardList />
       </div>
     );
   }
